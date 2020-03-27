0c454257fe42db6b5b8413b6b69b4319751add48
KMx404 <mohamedelfaleh5@gmail.com>
2020-03-27 12:00:57 -0400

    fresh

---
+++ Journey.py
@@ -0,0 +1,15 @@
+#! /usr/bin/env python3 
+
+def logo(): 
+    """
+    # The logo will represent the game and a signiture of the coder of course 
+
+    """
+
+def intro(): 
+    """
+    # The intro should contain the idea of the game. and how it should be played
+
+    """
+
+
---
+++ TheStory/TheStory.py
@@ -0,0 +1,6 @@
+#! /usr/bin/env python3 
+"""
+Here is gonna be the full story 
+
+
+"""
