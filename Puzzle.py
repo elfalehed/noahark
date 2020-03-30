e6da8ef1853a3a4db57ca29b3473c81c2da25d27
KMx404 <mohamedelfaleh5@gmail.com>
2020-03-30 09:17:41 -0400

    new idea

--- Puzzle.py
+++ Puzzle.py
@@ -1,20 +1,18 @@
 #! /usr/bin/env python3  # coding: UTF-8
 # This shit is coded by @KMx404, I guess you can tell by my github repository XD
 
-questions_prompt = [ 
-        ' Do you think you are  smart enough to excel NoahArk game ?\n a) Yes\n b) Maybe\n c) I dont know\n',
-        ' What is life ?\n a) Knowledge\n b) Art\n c) Freedom '   ] 
 
-questions_answer = [ 
-        "a", 
-        "a"
-                   ]       
-def run_test(questions_prompt, questions_answer): 
-    qu = 0 
-    for ques in questions_prompt: 
-        qu += 1 
-        answer = raw_input(questions_prompt[qu]) 
-        if answer == questions_answe[qu]: 
-            qu += 1 
+# The basic idea is gonna be like this : 
+def ask1(): 
+    pp = open("Prompts/p1.txt","r") 
+    answer = raw_input(pp.read())
+    if answer == "a" : 
+        print("Win")
+ask1()
 
-run_test(questions_prompt, questions_answer)
+
+
+
+
+
+
