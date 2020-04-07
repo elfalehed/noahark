98759e960e979784d2f71c9e81064cfc1b03cb3c
KMx404 <mohamedelfaleh5@gmail.com>
2020-04-07 14:18:03 -0400

    logo & intro

--- Journey.py
+++ Journey.py
@@ -1,16 +1,22 @@
 #! /usr/bin/env python3 # coding: UTF-8 
 
 # i need to view a tutorial on how to use tkinter (GUI) which will enable me of diving more and maybe providing something better as this game
-def logo(): 
-    """
-    # The logo will represent the game and a signiture of the coder of course 
+logo =    """
+       _   __            __    ___         __  
+   / | / /___  ____ _/ /_  /   |  _____/ /__
+  /  |/ / __ \/ __ `/ __ \/ /| | / ___/ //_/
+ / /|  / /_/ / /_/ / / / / ___ |/ /  / ,<   
+/_/ |_/\____/\__,_/_/ /_/_/  |_/_/  /_/|_|  
+                                        By: @KMx404 
 
-    """
+"""
+intro = """
+   Welcome to NoahArk! 
+   To get the full cover of this game check the story side. 
+   Are you ready to play ? 
 
-def intro(): 
-    """
-    # The intro should contain the idea of the game. and how it should be played
-
-    """
+"""
 
 
+print(logo)
+print(intro)
