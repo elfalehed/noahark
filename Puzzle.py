fb93de4a0faed911c8b6d0eb515df00e85782062
KMx404 <mohamedelfaleh5@gmail.com>
2020-05-11 15:03:37 -0400

    new

--- Puzzle.py
+++ Puzzle.py
@@ -1,35 +1,15 @@
-#! /usr/bin/env python3  # coding: UTF-8
-# This shit is coded by @KMx404, I guess you can tell by my github repository XD
-#2Do: 
-# Check how the return statement actually work. Cause the main problem starts over there.
+#! /usr/bin/env python3 # coding: UTF-8 
 
-# The basic idea is gonna be like this : 
+import os
 
-def ask1(score,pp,answer)  : 
-    pp = open("Prompts/challenge1/p1.txt","r") 
-    answer = input(pp.read()) 
-    if answer == "a" : 
-       score = score + 1 
-    return score #as int(into)               
-
-def ask2(score, pp2,answer): 
-    pp2 =  open("Prompts/challenge1/p2.txt","r") 
-    answer = input(pp2.read()) 
-    if answer == "b" : 
-        score = score + 1 
-    return score  #s int(binto) 
-
-score = 0 
-answer = "" 
-#print(score.ask1()) 
-pp = open("Prompts/challenge1/p1.txt", "r") 
-sc1 = ask1(score,pp,answer) 
-if answer=="a": 
-    print("Yes") 
-else:
-    print("No") 
-pp2 = open("Prompts/challenge1/p2.txt", "r") 
-sc2 = ask2(score, pp2,answer) 
-
+file_count = sum(len(files) for _, _, files in os.walk(r'Prompts/challenge1/'))
+i = 1 
+for j in range(file_count) : 
+       
+      pp = open ("Prompts/challenge1/p"+ str(i) +".txt","r") 
+      i = i + 1 
+      answer = input(pp.read()) 
+      if answer == "a": 
+        print("Fuck ya") 
 
 
