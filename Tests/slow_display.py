d150243ff2cba49003b5d9ea2755d7f7646f6404
KMx404 <mohamedelfaleh5@gmail.com>
2020-06-05 07:01:36 -0400

    test success

---
+++ Tests/slow_display.py
@@ -0,0 +1,17 @@
+#! /usr/bin/env python3 
+
+import sys, time, random
+
+typing_speed = 50
+def slow_type(str): 
+    for letter in str:
+        sys.stdout.write(letter) 
+        sys.stdout.flush()
+        time.sleep(0.1) 
+
+slow_type("\ntest this gonna need to be a full testing paragraph in order to see the full potential *wrong grammar?*") 
+
+
+
+
+
