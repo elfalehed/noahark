76425397c815d46cc12683d1a7f7c458bfca4be8
KMx404 <mohamedelfaleh5@gmail.com>
2020-06-20 16:34:23 -0400

    not sure of the second puzzle && i think i do need a better strategy

--- Puzzle.py
+++ Puzzle.py
@@ -1,14 +1,21 @@
 #! /usr/bin/env python3 # coding: UTF-8 
 
 import os
+import sys 
+import numpy as np 
 from playsound import playsound
 
-prompts_count  = sum(len(files) for _, _, files in os.walk(r'Prompts/challenge1/'))
-answers_count = sum(len(files) for _, _, files in os.walk(r'Prompts/answer1/')) 
+# opening the responsible files, I think i do need a better method! 
+prompts_count1 =  sum(len(files) for _, _, files in os.walk(r'Prompts/challenge1/'))
+prompts_count2 =  sum(len(files) for _, _, files in os.walk(r'Prompts/challenge2/'))
+answers_count1 = sum(len(files) for _, _, files in os.walk(r'Prompts/answer1/')) 
+answer_count2 =  sum(len(files) for _, _, files in os.walk(r'Prompts/answer2/'))  
 
-answers = ["a", "b", "c","45"] 
-e = len(answers)
-def puzz(): 
+answers1 = ["a", "b", "c","45"] 
+e = len(answers) 
+
+# Puzz1 
+def puzz1(): 
     i = 1 
     l = 1 
     c = 0 
@@ -20,6 +27,30 @@ def puzz():
           answer = input(pp.read() )
           aa = open ("Prompts/answer1/a" + str(i) +".txt", "r") 
           for k in range (e): 
+                if answer == answers1[c]: 
+                    score += 1 
+                    c = c + 1 
+                elif answer != answers1[c]: 
+                    pass
+                elif answers1[c] > len(answers): 
+                    break
+                else: 
+                    pass
+    print('Your score is =',score)     
+
+# Puzz2 
+def puzz2(): 
+    i = 1 
+    l = 1 
+    c = 0 
+    score = 0 
+    for j in range(prompts_count) : 
+          pp = open ("Prompts/challenge2/p"+ str(i) +".txt","r") 
+          # opening answers was here   
+          i = i + 1 
+          answer = input(pp.read() )
+          aa = open ("Prompts/answer2/a" + str(i) +".txt", "r") 
+          for k in range (e): 
                 if answer == answers[c]: 
                     score += 1 
                     c = c + 1 
@@ -30,14 +61,14 @@ def puzz():
                 else: 
                     pass
     print('Your score is =',score)    
-    playsound('noahark.mp3') 
-
+    
 
 def main(): 
     try:
-        puzz() 
+        puzz1() 
         letter = "test"
         print(letter, end='')  
+        playsound('noahark.mp3') 
     except: 
         print("What are you doing?") 
     # i need an errors handler over here 
